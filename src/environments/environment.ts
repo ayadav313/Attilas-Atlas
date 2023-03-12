b26d7b286f279251f9ae30d62fdd5aad29a9fa60
ayadav <123587824+ayadav313@users.noreply.github.com>
2023-03-12 08:51:11 -0400

    firebase connection

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'atlas-56e5b',
+    appId: '1:926156443016:web:05f1dc18f87dea7aafbe3d',
+    storageBucket: 'atlas-56e5b.appspot.com',
+    apiKey: 'AIzaSyCjEfH7UiBJ5OnUhYxE1t3YCk3XgBKNbB4',
+    authDomain: 'atlas-56e5b.firebaseapp.com',
+    messagingSenderId: '926156443016',
+    measurementId: 'G-1W163SH1RN',
+  }
+};
