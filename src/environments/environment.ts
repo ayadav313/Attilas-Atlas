8d36231ea8d3f67479a54a7693fd07b9e844bd0a
ayadav <123587824+ayadav313@users.noreply.github.com>
2023-05-02 10:12:52 -0400

    chore(environment.ts): replace firebase config values with placeholders

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: false,
   firebase: {
     projectId: 'atlas-56e5b',
-    appId: '1:926156443016:web:05f1dc18f87dea7aafbe3d',
+    appId: 'EXAMPLE-APP-ID',
     databaseURL: 'https://atlas-56e5b-default-rtdb.firebaseio.com',
     storageBucket: 'atlas-56e5b.appspot.com',
-    apiKey: 'AIzaSyCjEfH7UiBJ5OnUhYxE1t3YCk3XgBKNbB4',
+    apiKey: 'EXAMPLE-API-KEY',
     authDomain: 'atlas-56e5b.firebaseapp.com',
-    messagingSenderId: '926156443016',
+    messagingSenderId: 'EXAMPLE-SENDER-ID',
     measurementId: 'G-1W163SH1RN',
   }
 };
